7960ed1f174a2241f8730706823e7961307f4c10
Gwendal-B <12309@holbertonstudents.com>
2026-04-08 09:38:36 +0200

    fix: pycodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Executes async_comprehension four times in parallel and returns runtime."""
+    """Executes async_comprehension four
+    times in parallel and returns runtime."""
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return time.time() - start_time
