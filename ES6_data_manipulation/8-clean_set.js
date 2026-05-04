34ebc414af4812204e9e7c117a0b8105b15f081f
Gwendal-B <12309@holbertonstudents.com>
2026-05-04 12:48:02 +0200

    update task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
 
   return [...set]
     .filter((value) => typeof value === 'string' && value.startsWith(startString))
