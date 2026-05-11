eb88c7d2604abc1fd5a8aa7805236df2ca08a87c
Gwendal-B <12309@holbertonstudents.com>
2026-05-11 11:19:38 +0200

    update task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,7 +4,7 @@ function countStudents(path) {
   let data;
 
   try {
-    data =fs.readFileSync(path, 'utf8');
+    data = fs.readFileSync(path, 'utf8');
   } catch (error) {
     throw new Error('Cannot load the database');
   }
