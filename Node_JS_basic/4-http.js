50535a6ba914699fdf1cd302ae3ac755ed7a8739
Gwendal-B <12309@holbertonstudents.com>
2026-05-12 14:54:48 +0200

    Add \n for task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello Holberton School!');
+  response.end('Hello Holberton School!\n');
 });
 
 app.listen(1245);
