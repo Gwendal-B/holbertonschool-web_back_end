b5afe98c361b850cb63650c437140cb214beda94
Gwendal-B <12309@holbertonstudents.com>
2026-05-12 15:31:07 +0200

    added task 5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,70 @@
+const http = require('http');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+
+      const students = lines.slice(1);
+
+      const output = [];
+      output.push(`Number of students: ${students.length}`);
+
+      const fields = {};
+
+      students.forEach((student) => {
+        const [firstname, , , field] = student.split(',');
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+      });
+
+      Object.keys(fields).forEach((field) => {
+        const number = fields[field].length;
+        const list = fields[field].join(', ');
+
+        output.push(
+          `Number of students in ${field}: ${number}. List: ${list}`
+        );
+      });
+
+      resolve(output.join('\n'));
+    });
+  });
+}
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const database = process.argv[2];
+
+    res.statusCode = 200;
+
+    countStudents(database)
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((error) => {
+        res.end(`This is the list of our students\n${error.message}`);
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
